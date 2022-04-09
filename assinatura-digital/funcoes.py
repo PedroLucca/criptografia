4583610328c7af819827d40c4537d22a9d82f0ef
PedroLucca <pedrolucca27@gmail.com>
2022-04-08 22:05:27 -0300

    Ainda falta finalizar

--- assinatura-digital/funcoes.py
+++ assinatura-digital/funcoes.py
@@ -2,6 +2,9 @@ from Crypto.Hash import SHA1
 from Crypto.Hash import SHA256
 from Crypto import Random
 from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+import base64
+import pickle
 
 class Funcoes:
     def __init__(self):
@@ -10,11 +13,22 @@ class Funcoes:
         self.assinatura = ''
         self.geraKeys()
 
-    def pkcs5_pad(self, s, BLOCK_SIZE=16):   
-        print(s)                                                                                                                                                           
-        return (s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(                                                                                                                                     
-            BLOCK_SIZE - len(s) % BLOCK_SIZE                                                                                                                                                      
-            )).encode('utf-8')
+    def verifica(self, exported_key):
+        data = self.assinatura[0]
+        signature = self.assinatura[1]
+        key = RSA.importKey(exported_key)
+        h = SHA256.new(pickle.dumps(data))
+        return PKCS1_v1_5.new(key).verify(h, signature)
+
+    def assinar(self, data, exported_key):
+        key = RSA.importKey(exported_key)
+        signed_data = []
+        signed_data.append(data)
+        signed_data.append(PKCS1_v1_5.new(key).sign(SHA256.new(pickle.dumps(data))))
+        with open("assinatura.txt", "wb") as f:
+            f.write(b"--- ASSINATURA BASE64 ---\n")
+            f.write(base64.b64encode(signed_data[1]))
+        return signed_data
 
     def geraKeys(self):
         #Gera as chaves
@@ -23,10 +37,12 @@ class Funcoes:
         key = RSA.generate(tam_key, rand)
 
         private_key = key.exportKey()
+        self.privateKey = private_key
         with open("privateKey.txt", "wb") as f:
             f.write(private_key)
 
         public_key = key.publickey().exportKey()
+        self.public_key = public_key
         with open("publicKey.txt", "wb") as f:
             f.write(public_key)
 
@@ -35,9 +51,13 @@ class Funcoes:
 
     def assinaDocumento(self, mensagem, tipoSHA=0):
         if tipoSHA == 0:
-            hash = SHA256.new(self.pkcs5_pad(mensagem)).digest()
-            self.assinatura = self.privateKey.sign(hash, '')
+            #hash = SHA256.new(mensagem.encode("utf-8")).digest()
+            privateKey = self.privateKey.exportKey()
+            self.assinatura = self.assinar(mensagem, privateKey)
 
     def verificaAutenticidade(self):
-        result = self.privateKey.publickey().verify(hash, self.assinatura)
-        print(result)
+        #self.assinatura[0] = 'Some dataaaaaaaa'             # Succeeds
+        self.assinatura[0] = 'Testando a criptogfia dessa mensagem que diz muita coisa'        # Fails
+        keyPub = self.privateKey.publickey().exportKey()
+        verified = self.verifica(keyPub)
+        print(verified)
--- assinatura-digital/main.py
+++ assinatura-digital/main.py
@@ -2,6 +2,37 @@ from funcoes import Funcoes
 
 crypt = Funcoes()
 
-crypt.assinaDocumento('Testando a criptografia dessa mensagem que diz muita coisa')
-crypt.verificaAutenticidade()
+while True:
+    print("\nTrabalho de Segurança - Assinatura Digital")
+    print("Alunos: Daniel Évani, Manassés Silva e Pedro Lucca\n")
+
+    print("--- MENU PRINCIPAL ---\n")
+    
+    print("1 - Assinar mensagem")
+    print("2 - Verificar assinatura")
+    print("3 - Gerar novas chaves")
+    print("0 - Sair\n")
+
+    opcao = int(input("Digite uma opção: "))
+
+    if opcao == 0:
+        print("\nPrograma finalizado...")
+        break
+    elif opcao == 1:
+        f = open("mensagem.txt", "r")
+        mensagem = f.read()
+
+        crypt.assinaDocumento(mensagem)
+        print(crypt.assinatura)
+    elif opcao == 2:
+        crypt.verificaAutenticidade()
+    elif opcao == 3:
+        crypt.geraKeys()
+        print("\n Chaves também se encontram nos respectivos arquivos txt!")
+        
+
+        """ 
+        
+        print(crypt.assinatura)
+        crypt.verificaAutenticidade() """
 

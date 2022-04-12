39633076711ff276478f3f297f1585d78dbfe1fc
PedroLucca <pedrolucca27@gmail.com>
2022-04-11 22:48:20 -0300

    Trabalho finalizado

--- assinatura-digital/funcoes.py
+++ assinatura-digital/funcoes.py
@@ -13,9 +13,6 @@ from regex import P
 
 class Funcoes:
     def __init__(self):
-        self.publicKey = ''
-        self.privateKey = ''
-        self.assinatura = ''
         self.geraKeys()
 
     def verifica(self, exported_key, mensagem, assinatura, tipoSHA):
@@ -26,16 +23,15 @@ class Funcoes:
         if tipoSHA == 1:
             h = SHA256.new(pickle.dumps(data))
         elif tipoSHA == 2:
-            h = SHA256.new(pickle.dumps(data))
+            h = SHA1.new(pickle.dumps(data))
         elif tipoSHA == 3:
-            h = SHA256.new(pickle.dumps(data))
+            h = SHA224.new(pickle.dumps(data))
         elif tipoSHA == 4:
-            h = SHA256.new(pickle.dumps(data))
+            h = SHA384.new(pickle.dumps(data))
         elif tipoSHA == 5:
-            h = SHA256.new(pickle.dumps(data))
+            h = SHA512.new(pickle.dumps(data))
         else:
             h = SHA256.new(pickle.dumps(data))
-
 
         return PKCS1_v1_5.new(key).verify(h, signature)
 
@@ -57,7 +53,6 @@ class Funcoes:
             signed_data.append(PKCS1_v1_5.new(key).sign(SHA256.new(pickle.dumps(data))))
         with open("assinatura.txt", "wb") as f:
             f.write(base64.b64encode(signed_data[1]))
-        print("\nAssinatura: ", base64.b64encode(signed_data[1]).decode('utf-8'))
         return signed_data
 
     def geraKeys(self):
@@ -67,22 +62,18 @@ class Funcoes:
         key = RSA.generate(tam_key, rand)
 
         private_key = key.exportKey()
-        self.privateKey = private_key
         with open("privateKey.txt", "wb") as f:
             f.write(private_key)
 
         public_key = key.publickey().exportKey()
-        self.public_key = public_key
         with open("publicKey.txt", "wb") as f:
             f.write(public_key)
 
-        self.privateKey = key
-
     def assinaDocumento(self, mensagem, chave, tipoSHA=1):
-        self.assinatura = self.assinar(mensagem, chave, tipoSHA)
+        assinatura = self.assinar(mensagem, chave, tipoSHA)
+        print("\nAssinatura: ", base64.b64encode(assinatura[1]).decode('utf-8'))
 
     def verificaAutenticidade(self, mensagem, publicKey, assinatura, tipoSHA=1):
-    
         assinatura = base64.b64decode(assinatura)
         verified = self.verifica(publicKey, mensagem, assinatura, tipoSHA)
         print("\nVerificação da mensagem: ", verified)
